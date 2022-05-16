0e65c064649af25cf26c897e1361d043b4d4508e
CBX0N <88788571+CBX0N@users.noreply.github.com>
2022-05-16 17:00:43 +0100

    main

---
+++ media-stack-run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Creating Basic Paths"
+mkdir /media-stack
+mkdir /media-stack/conf
+mkdir /media-stack/data
+
+
+echo "Creating Config Paths"
+mkdir /media-stack/conf/radarr
+mkdir /media-stack/conf/jackett
+
+echo "Creating Media Paths"
+mkdir /media-stack/data/downloads/torrents
+mkdir /media-stack/data/Movies
+
+docker-compose up --detach 
